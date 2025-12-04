7f83c4abc358bf4b99fd93a27c9349469ec9a50e
awe123343 <awe1233430@hotmail.com>
2025-12-04 12:02:45 -0800

    feat(ui): nvchad ui

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,30 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.nvchad-ui" },
+  { import = "astrocommunity.icon.mini-icons" },
+  -- Configure NvChad UI
+  {
+    "NvChad/ui",
+    opts = {
+      base46 = {
+        theme = "everblush",
+      },
+      ui = {
+        statusline = { enabled = false }, -- Using lualine instead
+        tabufline = {
+          lazyload = false,
+          enabled = true,
+        },
+      },
+    },
+  },
+  -- Disable heirline statusline but keep statuscolumn for folding
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      opts.statusline = nil -- Disable heirline statusline, using lualine
+      return opts
+    end,
+  },
 }
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,16 +8,12 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
+    -- Don't set colorscheme - let base46 handle all theming
+    colorscheme = false,
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    -- Neo-tree highlights are handled in polish.lua to ensure they override astrodark
     highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
+      init = {},
     },
     -- Icons can be configured throughout the interface
     icons = {
